a214c5e05a73e9a3641aa25ae27a2063086c8b6e
moxirboy <mox777boy@gmail.com>
2024-02-19 21:48:17 +0500

    added test migration

--- migrations/000001_user.up.sql
+++ migrations/000001_user.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR(255) NOT NULL,
+    "firstname" VARCHAR(255) NOT NULL,
+    "lastname" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "role" VARCHAR(255) NOT NULL DEFAULT 'user',
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
